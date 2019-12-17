2cbe4b976bcb1c8add9cd8c856fa8b86850269aa
Nethelbert <gilblasse@gmail.com>
2019-12-17 20:17:56 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -21,7 +21,7 @@ class App < Sinatra::Base
   end
   
   get '/say/:word1/:word2/:word3/:word4/:word5' do 
-    params.map {|k,v| v }.join(' ')
+    "#{params.map {|k,v| v }.join(' ')}."
   end
   
   get '/:operation/:number1/:number2' do 
