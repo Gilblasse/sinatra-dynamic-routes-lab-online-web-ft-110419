3ed3c8cd20f5d9f5c7c405a21fcd0db4e7e51300
Nethelbert <gilblasse@gmail.com>
2019-12-17 19:47:58 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -19,9 +19,9 @@ class App < Sinatra::Base
     @num.times {"#{@phrase}"}
   end
   
-  get '/square/:number' do 
-    @num = params[:number].to_i
-    "#{@num * @num}"
+  get '/say/:word1/:word2/:word3/:word4/:word5' do 
+    @word1 = params[:word1]
+    
   end
 
 end
