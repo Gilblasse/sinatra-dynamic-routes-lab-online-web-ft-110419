4d16a06195609ca41a78fb5940412acf1d52498a
Nethelbert <gilblasse@gmail.com>
2019-12-17 20:28:13 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -25,7 +25,9 @@ class App < Sinatra::Base
   end
   
   get '/:operation/:number1/:number2' do 
-    params[:number1]
+    @num1 = params[:number1]
+    @num2 = params[:number2]
+    @op = [:operation]
     binding.pry
     case params[:operation]
     when 'add'
